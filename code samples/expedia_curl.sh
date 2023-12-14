74ed7bce4dfe2b1e8f951dec41ff7703a66a4ebe
OxylabsOrg <103110131+OxylabsOrg@users.noreply.github.com>
2023-12-14 17:46:32 +0200

    Create expedia_curl.sh

---
+++ code samples/expedia_curl.sh
@@ -0,0 +1,4 @@
+curl --user USERNAME:PASSWORD \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal", "url": "https://www.expedia.de/Hotel-Search?adults=2&d1=2024-07-04&d2=2024-07-06&destination=Frankfurt%2C%20Deutschland%20%28FRA-Frankfurt%20Intl.%29&endDate=2024-07-06&flexibility=7_DAY&latLong=50.050978%2C8.571705&regionId=4280902&rooms=1&semdtl=&sort=RECOMMENDED&startDate=2024-07-04&theme=&useRewards=false&userIntent=", "geo_location": "Germany"}'
